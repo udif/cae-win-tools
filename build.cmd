d4f3d230447e6dacfabdbb88599655ea2bbbef5c
Udi Finkelstein (udif) <github@udifink.com>
2017-01-31 04:51:35 +0200

    Added support for gtkwave build (currently disabled). Also build centos7 base image for cvc devel

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 docker build -t udif/cae-build-base dockerfiles/cae-build-base
+docker build -t udif/cae-build-base-centos7 dockerfiles/cae-build-base-centos7
 if x%1x == xiverilogx (
 	docker build -t udif/icarus-win dockerfiles/icarus-win
 	docker create --name icarus_win udif/icarus-win
@@ -17,12 +18,20 @@ if x%1x == xarachnex (
 	docker cp arachne_win:/zip .
 	docker rm arachne_win
 )
+@rem if x%1x == xgtkwavex (
+@rem	docker build -t udif/gtkwave-win dockerfiles/gtkwave-win
+@rem	docker create --name gtkwave_win udif/gtkwave-win
+@rem	docker cp gtkwave_win:/zip .
+@rem	docker rm gtkwave_win
+@rem )
 if x%1x == xcleanx (
 	docker rm icarus_win
 	docker rm yposys_win
 	docker rm arachne_win
+@rem	docker rm gtkwave_win
 	docker rmi udif/icarus-win
 	docker rmi udif/yosys-win
 	docker rmi udif/arachne-win
+@rem	docker rmi udif/gtkwave-win
 	docker rmi udif/cae-build-base
 )
